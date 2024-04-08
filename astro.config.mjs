a0cd26439be968cb11768cbcec9e99cde42a8ee6
Jeremy Pittard <jeremy@jpittard.net>
2024-04-08 11:18:00 +0800

    disallow

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,13 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [db(), tailwind({ applyBaseStyles: true }), react(), robotsTxt()],
+  integrations: [db(), tailwind({ applyBaseStyles: true }), react(), robotsTxt({
+    policy: [
+      {
+        userAgent: '*',
+          Disallow: '/',
+      }
+    ]
+  })],
   adapter: vercel(),
 });
